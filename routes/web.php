e108191d40873bed98aa1c67e84fdeac5d4496bc
Felipe CÂndido <felipe.candido8@gmail.com>
2024-05-03 20:49:59 -0300

    Saída rapida guarda os dados no form para facilitar entrada em massa

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
 
 Auth::routes();
 
